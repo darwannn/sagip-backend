5fbd1a7285ec2ab85d791526be7e26f36dd25959
darwin <darwinsanluisramos21@gmail.com>
2023-05-27 11:09:01 +0800

    added safety tips toast

--- backend/controllers/safetyTipController.js
+++ backend/controllers/safetyTipController.js
@@ -1,14 +1,38 @@
 const safetyTipController = require("express").Router()
 const SafetyTip = require("../models/SafetyTip")
 const verifyToken = require('../middlewares/verifyToken')
-
+const { isEmpty } = require('./functionController')
 
 safetyTipController.post('/add', verifyToken, async (req, res) => {
+    const error = {};
     try {
-        const safetyTip = await SafetyTip.create({ ...req.body, userId: req.user.id })
-        return res.status(201).json({ message: 'SafetyTip created successfully', safetyTip })
+        const {title, content,category, image} = req.body;
+         if (isEmpty(title)) error["title"] = 'Required field'
+         if (isEmpty(content)) error["content"] = 'Required field'
+         if (isEmpty(category)) error["category"] = 'Required field'
+
+         if (Object.keys(error).length == 0) {
+
+             const safetyTip = await SafetyTip.create({ ...req.body, userId: req.user.id })
+             return res.status(200).json({
+                success: true,
+                message: "SafetyTip created successfully",
+              });
+         }
+  
+        if (Object.keys(error).length != 0) {
+   
+            error["success"] = false;
+            error["message"] = "input error";
+        
+            return res.status(400).json(error)
+            
+          }
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error" + error,
+          })
     }
 })
 /* get all */
@@ -19,7 +43,10 @@ safetyTipController.get('/', async (req, res) => {
         console.log(safetyTips);
         return res.status(200).json(safetyTips)
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error" + error,
+          })
     }
 })
 
@@ -33,31 +60,54 @@ safetyTipController.get('/:id', async (req, res) => {
         await safetyTip.save()
         return res.status(200).json(safetyTip)
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error" + error,
+          })
     }
 })
 
 
 
 safetyTipController.put("/update/:id", verifyToken, async (req, res) => {
-    try {
-     console.log('====================================');
-     console.log("Dasdas");
-     console.log('====================================');
-     
-        const safetyTip = await SafetyTip.findById(req.params.id)
-        if (safetyTip.userId.toString() !== req.user.id.toString()) {
-            throw new Error("You can update only your own posts")
-        }
+   
 
-        const updatedSafetyTip = await SafetyTip
-            .findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
-            .populate('userId', '-password')
-
-        return res.status(200).json({ message: 'SafetyTip updated successfully', updatedSafetyTip })
-    } catch (error) {
-        return res.status(500).json(error.message)
-    }
+          const error = {};
+          try {
+              const {title, content,category, image} = req.body;
+               if (isEmpty(title)) error["title"] = 'Required field'
+               if (isEmpty(content)) error["content"] = 'Required field'
+               if (isEmpty(category)) error["category"] = 'Required field'
+      
+               if (Object.keys(error).length == 0) {
+    
+            /*     const safetyTip = await SafetyTip.findById(req.params.id)
+                if (safetyTip.userId.toString() !== req.user.id.toString()) {
+                    throw new Error("You can update only your own posts")
+                } */
+        
+                const updatedSafetyTip = await SafetyTip
+                    .findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+                    .populate('userId', '-password')
+        
+                return res.status(200).json({
+                    success: true,
+                    message: "Safety Tip updated successfully",
+                  });
+               }
+        
+              if (Object.keys(error).length != 0) {
+                  error["success"] = false;
+                  error["message"] = "input error";
+                  return res.status(400).json(error)
+                  
+                }
+          } catch (error) {
+              return res.status(500).json({
+                  success:false,
+                  message:"Internal Server Error" + error,
+                })
+          }
 })
 
 /* safetyTipController.put('/saves/:id', verifyToken, async (req, res) => {
@@ -76,22 +126,36 @@ safetyTipController.put("/update/:id", verifyToken, async (req, res) => {
         }
 
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json({
+        success:false,
+        message:"Internal Server Error" + error,
+      })
     }
 }) */
 
 safetyTipController.delete('/delete/:id', verifyToken, async(req, res) => {
+    
+
+
+  
+    
     try {
         const safetyTip = await SafetyTip.findById(req.params.id)
-        if(safetyTip.userId.toString() !== req.user.id.toString()){
+       /*  if(safetyTip.userId.toString() !== req.user.id.toString()){
             throw new Error("You can delete only your own posts")
-        }
+        } */
         
         await SafetyTip.findByIdAndDelete(req.params.id)
+        return res.status(200).json({
+            success: true,
+            message: "SafetyTip deleted successfully",
+          });
 
-        return res.status(200).json({message: "Successfully deleted the safetyTip"})
     } catch (error) {
-        return res.status(500).json(error)
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error" + error,
+          })
     }
 })
 safetyTipController.get('/saved/:userId', async (req, res) => {
@@ -99,7 +163,10 @@ safetyTipController.get('/saved/:userId', async (req, res) => {
       const likedSafetyTips = await SafetyTip.find({ saves: req.params.userId }).populate("userId", "-password");
       return res.status(200).json(likedSafetyTips);
     } catch (error) {
-      return res.status(500).json(error);
+        return res.status(500).json({
+            success:false,
+            message:"Internal Server Error" + error,
+          })
     }
   });
   
