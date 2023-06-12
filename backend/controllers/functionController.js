4a6d0fec0a71d86edfe2f5ac571d1fcd8e571d7e
darwin <darwinsanluisramos21@gmail.com>
2023-06-12 22:58:19 +0800

    fix

--- backend/controllers/functionController.js
+++ backend/controllers/functionController.js
@@ -118,10 +118,10 @@ const checkIdentifier = async (identifier) => {
   } else if (/^09\d{9}$/.test(identifier)) {
     identierType = "contactNumber";
   } else {
-    return res.status(400).json({
+    /*   return res.status(400).json({
       success: true,
       message: "Invalid Email or Contact NUmber",
-    });
+    }); */
   }
   const accountExists = await User.findOne({
     [identierType]: identifier,
